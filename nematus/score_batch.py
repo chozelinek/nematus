ec90c1a91cbabd352bf035d3ed2782e3f108ddc3
José Manuel Martínez Martínez <chozelinek@gmail.com>
2017-06-25 19:27:17 +0200

    implement score files in batch mode

--- nematus/score_batch.py
+++ nematus/score_batch.py
@@ -7,6 +7,9 @@ import sys
 import argparse
 import tempfile
 
+import os
+import fnmatch
+
 import numpy
 import json
 
@@ -21,7 +24,7 @@ from nmt import (pred_probs, build_model, prepare_data, init_params)
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 import theano
 
-def rescore_model(source_file, target_file, saveto, models, options, b, normalization_alpha, verbose, alignweights):
+def rescore_model(source_file, glob_pattern, target_file, saveto, models, options, b, normalization_alpha, verbose, alignweights):
 
     trng = RandomStreams(1234)
 
@@ -63,37 +66,66 @@ def rescore_model(source_file, target_file, saveto, models, options, b, normaliz
 
         return scores, alignments
 
-    pairs = TextIterator(source_file.name, target_file.name,
-                    options[0]['dictionaries'][:-1], options[0]['dictionaries'][-1],
-                     n_words_source=options[0]['n_words_src'], n_words_target=options[0]['n_words'],
-                     batch_size=b,
-                     maxlen=float('inf'),
-                     sort_by_length=False) #TODO: sorting by length could be more efficient, but we'd want to resort after
+    sfiles = get_files(source_file, glob_pattern)
+    for sfilepath in sfiles:
+        fbasename = os.path.basename(sfilepath)
+        print fbasename
+        tfilepath = os.path.join(target_file, fbasename)
+        ofilepath = os.path.join(saveto, fbasename)
+        with open(sfilepath, 'r') as sfile, open(tfilepath, 'r') as tfile, open(ofilepath, 'w') as ofile:
+            pairs = TextIterator(
+                sfile.name,
+                tfile.name,
+                options[0]['dictionaries'][:-1],
+                options[0]['dictionaries'][-1],
+                n_words_source=options[0]['n_words_src'],
+                n_words_target=options[0]['n_words'],
+                batch_size=b,
+                maxlen=float('inf'),
+                sort_by_length=False) #TODO: sorting by length could be more  efficient, but we'd want to resort after
 
-    scores, alignments = _score(pairs, alignweights)
+            scores, alignments = _score(pairs, alignweights)
 
-    source_file.seek(0)
-    target_file.seek(0)
-    source_lines = source_file.readlines()
-    target_lines = target_file.readlines()
+            sfile.seek(0)
+            tfile.seek(0)
+            source_lines = sfile.readlines()
+            target_lines = tfile.readlines()
 
-    for i, line in enumerate(target_lines):
-        score_str = ' '.join(map(str,[s[i] for s in scores]))
-        if verbose:
-            saveto.write('{0} '.format(line.strip()))
-        saveto.write('{0}\n'.format(score_str))
+            for i, line in enumerate(target_lines):
+                score_str = ' '.join(map(str,[s[i] for s in scores]))
+                if verbose:
+                    ofile.write('{0} '.format(line.strip()))
+                ofile.write('{0}\n'.format(score_str))
 
-    ### optional save weights mode.
-    if alignweights:
-        ### writing out the alignments.
-        temp_name = saveto.name + ".json"
-        with tempfile.NamedTemporaryFile(prefix=temp_name) as align_OUT:
-            for line in all_alignments:
-                align_OUT.write(line + "\n")
-            ### combining the actual source and target words.
-            combine_source_target_text_1to1(source_file, target_file, saveto.name, align_OUT)
+            ### optional save weights mode.
+            if alignweights:
+                ### writing out the alignments.
+#                 temp_name = os.path.splitext(fbasename)[0] + ".json"
+                temp_name = ofile.name + ".json"
+                with tempfile.NamedTemporaryFile(prefix=temp_name) as align_OUT:
+#                     for line in all_alignments:
+                    for line in alignments:
+#                         align_OUT.write(line + "\n")
+                        align_OUT.write("\n".join(line) + "\n")
+                    ### combining the actual source and target words.
+                    combine_source_target_text_1to1(sfile, tfile, ofile.name, align_OUT)
 
-def main(models, source_file, nbest_file, saveto, b=80,
+
+def get_files(directory, fileclue):
+        """Get all files in a directory matching a pattern.
+
+        Keyword arguments:
+        directory -- a string for the input folder path
+        fileclue -- a string as glob pattern
+        """
+        matches = []
+        for root, dirnames, filenames in os.walk(directory):
+            for filename in fnmatch.filter(filenames, fileclue):
+                matches.append(os.path.join(root, filename))
+        return matches
+
+
+def main(models, source_file, glob_pattern, nbest_file, saveto, b=80,
          normalization_alpha=0.0, verbose=False, alignweights=False):
 
     # load model model_options
@@ -103,7 +135,11 @@ def main(models, source_file, nbest_file, saveto, b=80,
 
         fill_options(options[-1])
 
-    rescore_model(source_file, nbest_file, saveto, models, options, b, normalization_alpha, verbose, alignweights)
+    if not os.path.exists(saveto):
+        os.makedirs(saveto)
+
+    rescore_model(source_file, glob_pattern, nbest_file, saveto, models, options, b, normalization_alpha, verbose, alignweights)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -114,19 +150,21 @@ if __name__ == "__main__":
     parser.add_argument('-v', action="store_true", help="verbose mode.")
     parser.add_argument('--models', '-m', type=str, nargs = '+', required=True,
                         help="model to use. Provide multiple models (with same vocabulary) for ensemble decoding")
-    parser.add_argument('--source', '-s', type=argparse.FileType('r'),
+    parser.add_argument('--source', '-s',
                         required=True, metavar='PATH',
                         help="Source text file")
-    parser.add_argument('--target', '-t', type=argparse.FileType('r'),
+    parser.add_argument('--target', '-t',
                         required=True, metavar='PATH',
                         help="Target text file")
-    parser.add_argument('--output', '-o', type=argparse.FileType('w'),
+    parser.add_argument('--output', '-o',
                         default=sys.stdout, metavar='PATH',
                         help="Output file (default: standard output)")
     parser.add_argument('--walign', '-w',required = False,action="store_true",
                         help="Whether to store the alignment weights or not. If specified, weights will be saved in <target>.alignment")
+    parser.add_argument('--glob_pattern', '-g', type=str, required=False, default="*.txt",
+                        help="glob pattern to select input files.")
 
     args = parser.parse_args()
 
-    main(args.models, args.source, args.target,
+    main(args.models, args.source, args.glob_pattern, args.target,
          args.output, b=args.b, normalization_alpha=args.n, verbose=args.v, alignweights=args.walign)
